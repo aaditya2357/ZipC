12498177a7903872e632f438ef011757fcfde7ed
aaditya2357 <aaditya2357@gmail.com>
2025-10-31 16:00:38 +0530

    index prisma

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,4 +1,6 @@
 import { Inngest } from "inngest";
+import { PrismaClient } from '@prisma/client';
+
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "Zip-crm" });
