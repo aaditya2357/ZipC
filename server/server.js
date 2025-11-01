b9c081ef70452dcd33e07f30fa55a8c6fc223442
aaditya2357 <aaditya2357@gmail.com>
2025-11-01 15:33:43 +0530

    just update

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
+import workspaceRouter from './routes/workspaceRoutes.js';
+import { protect } from './middlewares/authMiddleware.js';
+
 
 
 const app = express();
@@ -17,6 +20,8 @@ app.get('/', (req, res) => res.send('Server is live'));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
+// Routes
+app.use("/api/workspaces", protect, workspaceRouter)
 
 const PORT = process.env.PORT || 5000
 
