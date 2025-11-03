7ae71ed20d1ce1d52db8685a383940a271364a55
aaditya2357 <aaditya2357@gmail.com>
2025-11-03 09:37:26 +0530

    a

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import workspaceRouter from './routes/workspaceRoutes.js';
 import { protect } from './middlewares/authMiddleware.js';
+import projectRouter from './routes/projectRoutes.js';
 
 
 
@@ -22,6 +23,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Routes
 app.use("/api/workspaces", protect, workspaceRouter)
+app.use("/api/projects", protect, projectRouter)
 
 const PORT = process.env.PORT || 5000
 
